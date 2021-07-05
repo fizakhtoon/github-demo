3be836a51cb731b5c102b235188c15042673a9d1
fizakhtoon <86825458+fizakhtoon@users.noreply.github.com>
2021-07-05 17:15:57 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+# Add implementation
+def add(x,y):
+    return x+y
+    
+# Subtract implementation
+def subtract(x,y):
+    return x-y               #on Bug456
+    
+# Multiply implementation
+def multiply(x,y):
+    return x*y               #on master branch
+    
+# Divide implemenatation
+def divide(x,y):
+    if y == 0:               #on bug789
+        return devideby0error
+    else:
+        return x/y
+ 
+#square  
+def square(x):
+    pass
